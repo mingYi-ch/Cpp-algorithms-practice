3c9ffad94cf8bba809541d1b5553909e4845111a
Ming Yi <myi@student.ethz.ch>
2025-03-15 07:40:10 +0100

    Q27 - optimized

--- Q27.cpp
+++ Q27.cpp
@@ -2,19 +2,21 @@
 #include <vector>
 using namespace std;
 
+#include <vector>
+using namespace std;
+
 class Solution
 {
 public:
     int removeElement(vector<int> &nums, int val)
     {
-        size_t len = size(nums);
         size_t cnt = 0;
-        for (size_t i = 0; i < len; i++)
+        for (const int &elem : nums)
         {
-            if (nums[i] != val)
+            if (elem != val)
             {
                 // put value to the head
-                nums[cnt++] = nums[i];
+                nums[cnt++] = elem;
             }
         }
         return (cnt);
