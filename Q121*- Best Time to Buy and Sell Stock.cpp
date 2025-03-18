bb171515de719909dc52f5f14a6422fe38539c62
Ming Yi <myi@student.ethz.ch>
2025-03-18 11:57:01 +0100

    Q121: time complexity O(n), Kadane’s Algorithm

--- Q121*- Best Time to Buy and Sell Stock.cpp
+++ Q121*- Best Time to Buy and Sell Stock.cpp
@@ -3,23 +3,22 @@
 
 using namespace std;
 
+// find the dual: difference array; using Kadane’s Algorithm to find Maximum subarray
 class Solution
 {
 public:
     int maxProfit(vector<int> &prices)
     {
-        size_t len = prices.size();
+        const size_t len = prices.size();
         int max_profit = 0;
-        for (size_t i = 0; i < len; i++)
+        int current_profit = 0;
+
+        for (size_t i = 0; i < len - 1; i++)
         {
-            for (size_t j = i + 1; j < len; j++)
-            {
-                int profit = prices[j] - prices[i];
-                if (profit > max_profit)
-                {
-                    max_profit = profit;
-                }
-            }
+            int diff = prices[i + 1] - prices[i];
+
+            current_profit = max(diff, current_profit + diff);
+            max_profit = max(current_profit, max_profit);
         }
         return (max_profit);
     }
