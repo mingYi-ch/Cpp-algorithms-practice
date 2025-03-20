e6123b962d4d28ee88178c47d199c0333eb30088
Ming Yi <myi@student.ethz.ch>
2025-03-20 11:22:07 +0100

    Q122: solution

--- src/Q122*- Best Time to Buy and Sell Stock II.cpp
+++ src/Q122*- Best Time to Buy and Sell Stock II.cpp
@@ -4,59 +4,28 @@
 using namespace std;
 
 /*
-greedy algo:1. optimal solution includes the greedy choices;
-            2. optimal substructure
-proof by induction
+    consider the diff array, pick the positive; can sell and buy on the same day!
 */
 class Solution
 {
-private:
-    int subMaxProfit(vector<int> &prices, const int start, const int end)
-    {
-        if (start >= end)
-            return (0);
-
-        const size_t len = prices.size();
-        int max_profit = 0;
-        int current_profit = 0;
-
-        // start and end idx of optimal subarray
-        int pt1 = start;
-        int pt2 = start;
-
-        int pt1_current = start;
-        int pt2_current = start;
-
-        for (size_t i = start; i < end; i++) // i + 1 constraints
-        {
-            int diff = prices[i + 1] - prices[i];
-
-            current_profit = max(diff, current_profit + diff);
-            if (diff > current_profit + diff)
-            {
-                current_profit = diff;
-                pt1_current = i;
-                pt2_current = i + 1;
-            }
-            else
-            {
-                current_profit = diff + current_profit;
-                pt2_current = i + 1;
-            }
-
-            max_profit = max(current_profit, max_profit);
-            if (max_profit < current_profit)
-            {
-                pt1 = pt1_current;
-                pt2 = pt2_current;
-            }
-        }
-        return (max_profit + subMaxProfit(prices, start, pt1 - 1) + subMaxProfit(prices, pt2 + 1, end));
-    }
 
 public:
     int maxProfit(vector<int> &prices)
     {
-        return (prices, 0, prices.size() - 1);
+        if (prices.empty())
+        {
+            return 0;
+        }
+
+        int max_profit = 0;
+        for (size_t i = 0; i < prices.size() - 1; i++)
+        {
+            if (prices[i + 1] > prices[i])
+            {
+                max_profit += prices[i + 1] - prices[i];
+            }
+        }
+
+        return max_profit;
     }
 };
