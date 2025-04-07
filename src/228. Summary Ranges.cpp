78fe3a8af84f58d7ec6d420b53a6c56940ea91aa
Ming Yi <myi@student.ethz.ch>
2025-04-07 10:03:22 +0200

    20. ([)] is not valid by decription

---
+++ src/228. Summary Ranges.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<string> summaryRanges(vector<int> &nums)
+    {
+    }
+};
