568510e11df72904c7d93cea5cdb05e21d6dc027
Ming Yi <myi@student.ethz.ch>
2025-04-14 13:51:01 +0200

    add 104 solution

--- src/228. Summary Ranges.cpp
+++ src/228. Summary Ranges.cpp
@@ -9,5 +9,30 @@ class Solution
 public:
     vector<string> summaryRanges(vector<int> &nums)
     {
+        vector<string> ranges;
+        size_t left = 0, right = 0;
+        string interval;
+        while (left < nums.size())
+        {
+            while (right + 1 < nums.size() && nums[right] + 1 == nums[right + 1])
+            {
+                ++right;
+            }
+
+            if (left == right)
+            {
+                interval = to_string(nums[left]);
+            }
+            else
+            {
+
+                interval = to_string(nums[left]) + "->" + to_string(nums[right]);
+            }
+
+            ranges.push_back(interval);
+            ++right;
+            left = right;
+        }
+        return ranges;
     }
 };
